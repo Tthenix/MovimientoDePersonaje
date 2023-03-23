35c51c3990ccca14a82e2ee0eacec18eea3eb97d
Nahuel Quiroga <80957556+Tthenix@users.noreply.github.com>
2023-03-22 21:23:45 -0300

    feat: movimiento horizontal andando

---
+++ Assets/Movimientojugador.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimientojugador : MonoBehaviour
+{
+    public float speed = 4f;
+    int direction = 1;
+    bool isTouchingWall = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (isTouchingWall) {
+            direction *= -1;
+        }
+        
+        transform.Translate(Vector3.right * speed * direction * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Wall"))
+        {
+            isTouchingWall = true;
+            Debug.Log("Se ha detectado la colisión con la pared.");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Wall"))
+        {
+            isTouchingWall = false;
+        }
+    }
+}
